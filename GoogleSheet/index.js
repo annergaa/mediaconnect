92081aab20e8795fcea801b6c7dc9d4971e30f7c
annergaa <55230225+annergaa@users.noreply.github.com>
2023-03-27 19:27:38 +0200

    orderimport

--- GoogleSheet/index.js
+++ GoogleSheet/index.js
@@ -1,6 +1,7 @@
 const customerImport = require('../lib/customerImport')
 const subscriptionImport = require('../lib/subscriptionImport')
-const sendSlackMessage = require('../lib/slackApi')
+const sendSlackMessage = require('../lib/slackApi');
+const orderImport = require('../lib/orderImport');
 
 module.exports = async function (context, myTimer) {
     var timeStamp = new Date().toISOString();
@@ -12,6 +13,7 @@ module.exports = async function (context, myTimer) {
     await sendSlackMessage("Timer started");
     await customerImport();
     await subscriptionImport();
+    await orderImport();
     await sendSlackMessage("Timer ended");
 
     context.log('JavaScript timer trigger function ran!', timeStamp);   
